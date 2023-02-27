f004a0d82eaeb1fbf0eb623177e123a37522c8bd
Ashish Deshmukh <115146337+AshishDeshmukh02@users.noreply.github.com>
2023-02-27 11:38:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,12 @@
-// const getSumBtn = document.createElement("button");
-const getSumBtn = document.getElementById("ans")
-getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
+const calculateTotalBtn = document.getElementById('calculate-total-btn');
+		calculateTotalBtn.addEventListener('click', calculateTotal);
 
-	const getSum = () => {
-//Add your code here
-  // Get all the prices from the HTML table
-  const prices = document.querySelectorAll('.price');
-
-  // Initialize a variable to store the total price
-  let totalPrice = 0;
-
-  // Loop through each price and add it to the total price
-  for (let i = 0; i < prices.length; i++) {
-    totalPrice += parseFloat(prices[i].textContent);
-  }
-
-  // Create a new row to display the total price
-  const newRow = document.createElement('tr');
-  const totalCell = document.createElement('td');
-  totalCell.textContent = `Total Price: ${totalPrice.toFixed(2)}`;
-  newRow.appendChild(totalCell);
-
-  // Add the new row to the table
-  const table = document.querySelector('table');
-  table.appendChild(newRow);
-};
-
-getSumBtn.addEventListener("click", getSum);
-
+		function calculateTotal() {
+			const prices = document.querySelectorAll('.price');
+			let totalPrice = 0;
+			prices.forEach(price => {
+				totalPrice += parseFloat(price.textContent);
+			});
+			const totalPriceCell = document.getElementById('total-price');
+			totalPriceCell.textContent = totalPrice.toFixed(2);
+		}
